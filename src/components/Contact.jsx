cece2ef0b96d91f3de0ace149b3598c510a0e279
TaQsiimUwU <abdallahkassemhassan@gmail.com>
2025-08-29 02:21:12 +0300

    Updates

--- src/components/Contact.jsx
+++
@@ -1,7 +0,0 @@
-export const Contact = () => {
-  return (
-<div>
-    contact me here
-</div>
-  );
-}
--- src/components/Theme-Toggle.jsx
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-import { Sun ,Moon} from 'lucide-react';
-
-export const ThemeToggle = () => {
-  const [IsDark, setIsDark] = useState(true);
-
-  return <button> {IsDark ? <Sun size={40} absoluteStrokeWidth /> : <Moon />} </button>
-}
